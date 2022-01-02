fb8481561ba15c5e3e70d99b9ad78cebbeec8bdf
Gabriel Guarisa <gabrielguarisa@gmail.com>
2022-01-02 11:17:58 -0300

    Passando método da detecção como param

--- gurun/cv/detection.py
+++ gurun/cv/detection.py
@@ -17,6 +17,7 @@ class TemplateDetection(Node):
         target: Union[np.ndarray, str],
         threshold: float = 0.7,
         single_match: bool = False,
+        method: int = cv2.TM_CCOEFF_NORMED,
         *args: Any,
         **kwargs: Any,
     ) -> None:
@@ -33,6 +34,7 @@ class TemplateDetection(Node):
         self._target_width = int(target.shape[1])
         self._threshold = threshold
         self._single_match = single_match
+        self._method = method
 
     def __call__(
         self, image: Union[np.ndarray, str], *args: Any, **kwargs: Any
@@ -42,7 +44,7 @@ class TemplateDetection(Node):
             if image is None:
                 raise ValueError("Template Detection image file does not exist")
 
-        result = cv2.matchTemplate(image, self._target, cv2.TM_CCOEFF_NORMED)
+        result = cv2.matchTemplate(image, self._target, self._method)
 
         yloc, xloc = np.where(result >= self._threshold)
 
