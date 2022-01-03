70755eb1186e99d510413741abf1c49503ee7f8d
Gabriel Guarisa <gabrielguarisa@gmail.com>
2022-01-03 12:15:56 -0300

    Removing unnecessary code

--- gurun/gui/os.py
+++ gurun/gui/os.py
@@ -2,17 +2,13 @@ from typing import Any
 
 import subprocess
 
-from gurun import Node
 from gurun.node import WrapperNode
 
 
 class Subprocess(WrapperNode):
     def __init__(self, *popenargs: Any, **kwargs: Any):
         super().__init__(subprocess.run, **kwargs)
-        self._popenargs = popenargs
-
-    def run(self, *args: Any, **kwargs: Any) -> Any:
-        return super().run(*self._popenargs, *args, **kwargs)
+        self._args_memory = popenargs
 
 
 class Workspace(Subprocess):
